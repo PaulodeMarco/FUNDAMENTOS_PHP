b27f8efa55fbdc0be68a5af78adf017c25a3968d
PaulodeMarco <146986151+PaulodeMarco@users.noreply.github.com>
2023-10-25 14:41:20 -0300

    exercicios_laços_php

---
+++ Aula07_25.10/exercicos_lacos.php
@@ -0,0 +1,17 @@
+<?php
+$numero = 0;
+while ($numero <= 500) {
+    echo $numero . " ";
+    $numero += 2;
+}
+
+echo("<br>");
+echo("<br>");
+
+$numero = 499;
+while ($numero >= 1) {
+    echo $numero . " ";
+    $numero -= 2;
+}
+
+
